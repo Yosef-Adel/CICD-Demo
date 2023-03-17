326fd22a02c5925dfcb76a374b0dfca8e76f6b10
Yosef-Adel <yosef.saaid02@eng-st.cu.edu.eg>
2023-03-17 14:37:29 +0200

    test node server

--- roles/setup/files/index.js
+++ roles/setup/files/index.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var server = http.createServer(function (req, res) {
 res.writeHead(200);
-res.end("Hello world!");
+res.end("<h1>Hello World! This server is created by Yosef Adel with fully Automated methods </h1>");
 });
 server.listen(3000);
